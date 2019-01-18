dc4da6bc12c7a7c7c182853f04e0e9f6de30bb11
hy-kiera <bb.kiera@gmail.com>
2019-01-18 19:12:42 +0900

    get json

--- src/hy_spam_server.py
+++ src/hy_spam_server.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import os
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask_restful import Resource, Api
+import pickle
+import jsonpickle
 
 
 app = Flask(__name__) # create the application instance
@@ -10,23 +13,19 @@ api = Api(app)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=None,
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-# database connection
-def connect_db():
-    """Connects to the sqlite3"""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
 class HellowWorld(Resource):
     def get(self):
-        return {'hello': 'waasssss'}
+        with open ('../MyMessage', 'rb') as fp:
+            msg_list = pickle.load(fp)
+            print(msg_list[0])
+        return msg_list[0]
     
 api.add_resource(HellowWorld, '/')
 
